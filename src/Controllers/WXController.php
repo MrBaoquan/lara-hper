90998d8e4d47045891e9397451cfea7535e8ad5e
mrbaoquan <mrma617@gmail.com>
2021-12-30 16:48:32 +0800

    update jssdk api.

--- src/Controllers/WXController.php
+++ src/Controllers/WXController.php
@@ -3,18 +3,24 @@
 namespace Mrba\LaraHper\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Mrba\LaraHper\Controllers\ApiResponse\APIErrorCode;
 
 class WXController extends APIController
 {
     public function JSSDK(Request $request)
     {
-        $url = $request->input('url');
+        $input = $request->only('debug','url','jsApiList');
+        
+        $debug = Arr::get($input,'debug',false);
+        $jsApiList = Arr::get($input,'jsApiList',[]);
+        $url = Arr::get($input,'url');
+        
         $app = app('wechat.official_account');
         if (!isset($url)) {
-            return $this->responseError(APIErrorCode::InvalidParams);
+            return $this->responseError(APIErrorCode::InvalidParams,'单页应用, 不传url必然导致签名错误');
         }
         $app->jssdk->setUrl($url);
-        return $this->responseJson($app->jssdk->buildConfig(['scanQRCode'], false, false, false));
+        return $this->responseJson($app->jssdk->buildConfig($jsApiList, $debug, false, false));
     }
 }
